fbec579393c42d0e4f2f9fcf9834ffd3ebc2c642
Johan Andersson <joha@netcompany.com>
2018-03-07 21:19:18 +0100

    run in daemon mode

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+docker stop jenkins
 docker rm jenkins
 docker run -p 8080:8080 \
       -v /var/run/docker.sock:/var/run/docker.sock:ro \
       -v /usr/bin/docker:/usr/bin/docker \
-      -v /home/johan/.jenkins_ssh:/var/jenkins_home/.ssh_host/ \
+      -v /usr/share/jenkins-docker/.jenkins_ssh:/var/jenkins_home/.ssh_host/ \
+      -d \
       --restart=always \
       --network=host \
       --group-add 994 \
