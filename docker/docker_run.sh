dbfb0ef470bc85c2d63f72a4e51506b8f65221a1
Johan Andersson <joha@netcompany.com>
2018-03-26 20:31:46 +0000

    add groovy-langserver project

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,8 +6,7 @@ docker run -p 8080:8080 \
       -v /usr/bin/docker:/usr/bin/docker \
       -v /usr/share/jenkins-docker/.jenkins_ssh:/var/jenkins_home/.ssh_host/ \
       -d \
-      --restart=always \
       --network=host \
-      --group-add 994 \
+      --group-add 993 \
       --name jenkins \
       johan1a/jenkins
